6bb197b210324bc099d668a81e9cca918fbd5a48
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-03-14 11:26:09 +0000

    Cosmetic namespace fixes.

--- MbUnit.JavaScript/References/JavaScriptFileReference.cs
+++ MbUnit.JavaScript/References/JavaScriptFileReference.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MbUnit.JavaScript {
+namespace MbUnit.JavaScript.References {
     internal class JavaScriptFileReference : JavaScriptReference {
         public override IEnumerable<string> LoadAll() {
             throw new NotImplementedException();
