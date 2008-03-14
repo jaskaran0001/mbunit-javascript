6dd8f327bba00c39e7d35bbc40eb96ded5317818
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-03-14 12:00:19 +0000

    Basic support for file references (no relative references at this time).

--- MbUnit.JavaScript/References/JavaScriptFileReference.cs
+++ MbUnit.JavaScript/References/JavaScriptFileReference.cs
@@ -1,11 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace MbUnit.JavaScript.References {
     internal class JavaScriptFileReference : JavaScriptReference {
+        public string Path                  { get; private set; }
+        public string Pattern               { get; private set; }
+        public SearchOption SearchOption    { get; private set; }
+
+        public JavaScriptFileReference(string path, string pattern)
+            : this (path, pattern, SearchOption.TopDirectoryOnly)
+        {
+        }
+
+        public JavaScriptFileReference(string path, string pattern, SearchOption option) {
+            this.Path = path;
+            this.Pattern = pattern;
+            this.SearchOption = option;
+        }
+
         public override IEnumerable<string> LoadAll() {
-            throw new NotImplementedException();
+            var files = Directory.GetFiles(this.Path, this.Pattern, this.SearchOption);
+            foreach (var file in files) {
+                yield return File.ReadAllText(file);
+            }
         }
     }
 }
