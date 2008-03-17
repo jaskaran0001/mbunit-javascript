b2ff9bfe723bbe1c50a5d889d7cf1d5c2b16c14d
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-03-17 22:13:57 +0000

    External references fixed.

---
+++ trunk/MbUnit.JavaScript/Engines/Microsoft/IComScriptInvoker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Expando;
+using System.Text;
+
+namespace MbUnit.JavaScript.Engines.Microsoft {
+    internal interface IComScriptInvoker {
+        object Invoke(IExpando owner, string name, params object[] args);
+    }
+}
