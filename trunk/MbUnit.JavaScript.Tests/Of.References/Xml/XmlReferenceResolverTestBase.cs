6e62886d050b064570b3ecc99c39b9b8c1f298a9
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-03-19 21:08:55 +0000

    Basic parsing of <reference name="" assembly="" />.

---
+++ trunk/MbUnit.JavaScript.Tests/Of.References/Xml/XmlReferenceResolverTestBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.XPath;
+
+namespace MbUnit.JavaScript.Tests.Of.References.Xml {
+    public abstract class XmlReferenceResolverTestBase {
+        protected XPathNavigator GetReferencesXml(params string[] attributeStrings) {
+            var builder = new StringBuilder("<references>").AppendLine();
+            foreach (var attributeString in attributeStrings) {
+                builder.AppendFormat("<reference {0} />", attributeString).AppendLine();
+            }
+            builder.Append("</references>");
+
+            using (var reader = new StringReader(builder.ToString())) {
+                return new XPathDocument(reader)
+                                .CreateNavigator()
+                                .SelectSingleNode("references");
+            }
+        }
+    }
+}
