f94f1a1fdd48ed16e76fc0ffec178d9e36861c2d
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-03-19 21:08:55 +0000

    Basic parsing of <reference name="" assembly="" />.

--- MbUnit.JavaScript.Tests/Of.References/Xml/XmlPathReferenceResolverTest.cs
+++ MbUnit.JavaScript.Tests/Of.References/Xml/XmlPathReferenceResolverTest.cs
@@ -4,42 +4,30 @@ using System.IO;
 using System.Text;
 using System.Xml.XPath;
 
-using NMock2;
-
 using MbUnit.Framework;
 
 using MbUnit.JavaScript.References.Xml;
-using MbUnit.JavaScript.References;
 
 namespace MbUnit.JavaScript.Tests.Of.References.Xml {
     [TestFixture]
     [TestsOn(typeof(XmlPathReferenceResolver))]
-    public class XmlPathReferenceResolverTest : MockingTestBase {
+    public class XmlPathReferenceResolverTest : XmlReferenceResolverTestBase {
         [RowTest]
         [Row(@"C:\Test.js",   "",                     @"C:\Test.js",          Description = "Absolute path with no orignal path.")]
         [Row(@"C:\Test.js",   @"D:\",                 @"C:\Test.js",          Description = "Absolute path with original path.")]
         [Row(@"T2\Test.js",   @"C:\T1\Original.js",   @"C:\T1\T2\Test.js",    Description = "Relative path.")]
         public void TestLoadReferences(string referencePath, string originalPath, string expectedPath) {
             var resolver = new XmlPathReferenceResolver();
-            var xmlRoot = this.GetReferenceXml(referencePath);
+            var xmlRoot = this.GetPathReferencesXml(referencePath);
 
             var references = resolver.GetReferences(xmlRoot, JavaScriptReference.Files(originalPath, ""));
             
             CollectionAssert.AreElementsEqual(references, new[] { JavaScriptReference.Files(expectedPath, "") });
         }
 
-        private XPathNavigator GetReferenceXml(params string[] paths) {
-            var builder = new StringBuilder("<references>").AppendLine();
-            foreach (var path in paths) {
-                builder.AppendFormat("<reference path='{0}' />", path).AppendLine();
-            }
-            builder.Append("</references>");
-
-            using (var reader = new StringReader(builder.ToString())) {
-                return new XPathDocument(reader)
-                                .CreateNavigator()
-                                .SelectSingleNode("references");
-            }
+        private XPathNavigator GetPathReferencesXml(params string[] paths) {
+            var attributeStrings = Array.ConvertAll(paths, path => string.Format("path='{0}'", path));
+            return this.GetReferencesXml(attributeStrings);
         }
     }
 }
---
+++ MbUnit.JavaScript/References/Xml/XmlResourceReferenceResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.XPath;
+using System.Reflection;
+
+namespace MbUnit.JavaScript.References.Xml {
+    internal class XmlResourceReferenceResolver : IXmlReferenceResolver {
+        private const string DefaultAssemblyName = "System.Web.Extensions";
+
+        public bool CanGetReferences(JavaScriptReference original) {
+            return true;
+        }
+
+        public IEnumerable<JavaScriptReference> GetReferences(IXPathNavigable referencesRoot, JavaScriptReference original) {
+            var resourceNodes = referencesRoot.CreateNavigator().Select("reference[@name]");
+            foreach (XPathNavigator node in resourceNodes) {
+                yield return this.GetReference(node);
+            }
+        }
+
+        private JavaScriptResourceReference GetReference(XPathNavigator referenceNode) {
+            var assemblyName = referenceNode.GetAttribute("assembly", "");
+            if (string.IsNullOrEmpty(assemblyName))
+                assemblyName = DefaultAssemblyName;
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var assembly = Array.Find(assemblies, a => a.GetName().Name == assemblyName);
+            if (assembly == null) {
+                // TODO: Make it work when the System.Web.Extensions or other GAC assemblies are referenced
+                // by their partial name but not loaded in the current AppDomain. I should check what 
+                // Visual Studio does in these cases.
+                // Right now I can't even test System.Web.Extensions special case without mocking Assembly.Load,
+                // which is a bit of an overkill.
+
+                assembly = Assembly.Load(assemblyName);
+            }
+
+            var resourceName = referenceNode.GetAttribute("name", "");
+
+            return new JavaScriptResourceReference(resourceName, assembly);
+        }
+    }
+}
