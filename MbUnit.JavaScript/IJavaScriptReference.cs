442413a0f27b4de7fbd6b3e72d2b4358842b0af2
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-03-20 13:51:42 +0000

    Minor tweaks.

--- MbUnit.JavaScript/IJavaScriptReference.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MbUnit.JavaScript {
-    public interface IJavaScriptReference {
-        string LoadContent();
-    }
-}
