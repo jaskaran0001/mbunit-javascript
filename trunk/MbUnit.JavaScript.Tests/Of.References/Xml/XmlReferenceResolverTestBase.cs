1e3374ee21cb392df7dba06fbc60e738a0b2ba3a
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-03-21 14:45:00 +0000

    PathToResourceReferenceResolver fixed for original file names with dots.

--- trunk/MbUnit.JavaScript.Tests/Of.References/Xml/XmlReferenceResolverTestBase.cs
+++ trunk/MbUnit.JavaScript.Tests/Of.References/Xml/XmlReferenceResolverTestBase.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Xml.XPath;
 
 namespace MbUnit.JavaScript.Tests.Of.References.Xml {
-    public abstract class XmlReferenceResolverTestBase {
+    public abstract class XmlReferenceResolverTestBase : MockingTestBase {
         protected XPathNavigator GetReferenceXml(string attributeString) {
             string xml = string.Format("<reference {0} />", attributeString);
 
---
+++ trunk/MbUnit.JavaScript/References/Xml/Resources/IResourceLookup.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MbUnit.JavaScript.References.Xml.Resources {
+    internal interface IResourceLookup {
+        bool ResourceExists(string resourceName);
+    }
+}
