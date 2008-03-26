f0b3afd665bcb08c1b57b9cb08280d7991342ed0
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-03-26 13:30:02 +0000

    Copyright information updated.

--- trunk/MbUnit.JavaScript.Tests/Properties/AssemblyInfo.cs
+++ trunk/MbUnit.JavaScript.Tests/Properties/AssemblyInfo.cs
@@ -6,13 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MbUnit.JavaScript.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Handy Labs")]
 [assembly: AssemblyProduct("MbUnit.JavaScript.Tests")]
-[assembly: AssemblyCopyright("Copyright © Handy Labs 2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © Andrey Shchekin")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
--- trunk/MbUnit.JavaScript/Engines/Microsoft/Types/ThreadAwareComScriptObject.cs
+++ trunk/MbUnit.JavaScript/Engines/Microsoft/Types/ThreadAwareComScriptObject.cs
@@ -1,7 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/* 
+  Copyright (c) 2008-2009, Andrey Shchekin
+  All rights reserved.
+ 
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided
+  that the following conditions are met:
+    * Redistributions of source code must retain the above copyright notice, this list of conditions
+      and the following disclaimer.
+    
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+      and the following disclaimer in the documentation and/or other materials provided with the
+      distribution.
+ 
+    * Neither the name of the Andrey Shchekin nor the names of his contributors may be used to endorse or 
+      promote products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+  PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+  POSSIBILITY OF SUCH DAMAGE.
+*/
+
+using System;
 using System.Runtime.InteropServices.Expando;
-using System.Text;
 
 using MbUnit.JavaScript.Engines.Microsoft.Threading;
 
@@ -22,7 +46,7 @@ namespace MbUnit.JavaScript.Engines.Microsoft.Types
 
         public override bool TryGetValue(string key, out object value) {
             var result = Synchronize(() => {
-                object synchronizedValue = null;
+                object synchronizedValue;
                 var found = base.TryGetValue(key, out synchronizedValue);
                 
                 return new { Found = found, Value = synchronizedValue };
