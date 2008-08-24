33460e625175fd7c4fd426a36254a8d98e941456
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-08-24 16:46:37 +0000

    Custom toString formatter completed, removing json reference.

--- trunk/MbUnit.JavaScript/js/Core/Formatter.js
+++ trunk/MbUnit.JavaScript/js/Core/Formatter.js
@@ -1,11 +1,54 @@
-﻿/// <reference path="../External/json.js" />
-
+﻿MbUnit = MbUnit || {};
 MbUnit.Core = MbUnit.Core || {};
+
 MbUnit.Core.Formatter = {
-    toString : function(o) {
-        if (o === null)
+    toString : function(object) {
+        if (object === null)
             return "null";
+            
+        if (object === undefined)
+            return "undefined";
+            
+        if (typeof object.__MbUnit_Formatter_toString === 'function')
+            return object.__MbUnit_Formatter_toString(this);
+            
+        if (typeof object === 'object')
+            return this._objectToString(object);
     
-        return Object.toJSONString(o);
+        return object.toString();
+    },
+    
+    _objectToString : function(object) {
+        var result = ["{"];       
+        for (var key in object) {
+            result.push(" ", key, ": ", this.toString(object[key]), ",");
+        }
+       
+        if (result.length > 1) {
+            result.pop(); // removing last ','
+            result.push(" ");
+        }
+            
+        result.push("}");
+
+        return result.join('');
     }
+};
+
+Array.prototype.__MbUnit_Formatter_toString = function(formatter) {
+    var result = ['['];
+    for (var i = 0; i < this.length; i++) {
+        result.push(formatter.toString(this[i]), ", ");
+    }
+    
+    if (this.length > 0)
+        result.pop(); // removing last ','
+    
+    result.push(']');
+    
+    return result.join('');
+};
+
+String.prototype.__MbUnit_Formatter_toString = function() {
+    return '"' + this + '"';
 };
