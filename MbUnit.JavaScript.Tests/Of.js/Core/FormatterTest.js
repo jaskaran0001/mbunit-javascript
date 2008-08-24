922d1b31c12a31fa4274f607de694c5a20271953
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-08-24 16:46:37 +0000

    Custom toString formatter completed, removing json reference.

--- MbUnit.JavaScript.Tests/Of.js/Core/FormatterTest.js
+++ MbUnit.JavaScript.Tests/Of.js/Core/FormatterTest.js
@@ -4,15 +4,19 @@ with (MbUnit.Core) {
 
 var FormatterTest = TestFixture({
     testToString : RowTest(
-        Row(1,                        "1"),
-        Row("x",                      '"x"'),
-        Row(true,                     "true"),
-        Row(null,                     "null"),
-        Row(undefined,                "undefined"),
-        Row([1, 2],                   "[1,2]"),
-        Row({name: 'test'},           '{"name":"test"}'),
-        //todo:Row(function() {},            'function() {}'), 
-        //todo:Row({ test: function() {}},   '{"test":function() {}}'),
+        Row(1,                      "1"),
+        Row("x",                    '"x"'),
+        Row(true,                   "true"),
+        Row(null,                   "null"),
+        Row(undefined,              "undefined"),
+        Row(NaN,                    'NaN'),
+        Row(function() {},          'function() {}'),
+        Row([],                     "[]"),        
+        Row([1, 2],                 "[1, 2]"),
+        Row({name: 'test'},         '{ name: "test" }'),
+        Row({},                     "{}"),
+        Row({ test: function() {}}, '{ test: function() {} }'),
+        Row({ test: [1,2,{}]},       '{ test: [1, 2, {}] }'),
         function(value, expected) {
             var string = Formatter.toString(value);
             Assert.areEqual(expected, string);            
