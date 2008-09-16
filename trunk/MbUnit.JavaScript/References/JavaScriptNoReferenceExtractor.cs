bfab0e3940798c459b0a63b343452ff7a8ea733c
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-09-16 19:40:00 +0000

    Added 'null' reference extractor (does nothing, to suppress the default extractor).

---
+++ trunk/MbUnit.JavaScript/References/JavaScriptNoReferenceExtractor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MbUnit.JavaScript.References {
+    public class JavaScriptNoReferenceExtractor : IJavaScriptReferenceExtractor {
+        public IEnumerable<IJavaScriptReference> GetReferences(IJavaScriptReference originalReference, string scriptContent) {
+            yield break;
+        }
+    }
+}
