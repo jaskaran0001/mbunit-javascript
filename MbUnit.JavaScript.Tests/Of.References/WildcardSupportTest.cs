5807d7dad1c80756dde0bf46297f46c2a29d64d4
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2008-10-02 19:17:55 +0000

    Basic (slow and naive) wildcards support.

---
+++ MbUnit.JavaScript.Tests/Of.References/WildcardSupportTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MbUnit.Framework;
+using MbUnit.JavaScript.References;
+
+namespace MbUnit.JavaScript.Tests.Of.References {
+    [TestFixture]
+    public class WildcardSupportTest {
+        [RowTest]
+        [Row("Test", false)]
+        [Row("Test*", true)]
+        [Row("*Test", true)]
+        public void TestHasWildcards(string pattern, bool expected) {
+            Assert.AreEqual(expected, new WildcardSupport().HasWildcards(pattern));
+        }
+
+        [RowTest]
+        [Row(@"D:\Development\*Test", @"D:\Development\")]
+        [Row(@"D:\Development\Test*", @"D:\Development\")]
+        [Row(@"D:\Development*Test",  @"D:\")]
+        [Row(@"D:/Development/*Test", @"D:/Development/")]
+        public void TestGetFixedRoot(string pattern, string expectedResult) {
+            Assert.AreEqual(
+                expectedResult, new WildcardSupport('/', '\\').GetFixedRoot(pattern)
+            );
+        }
+
+        [RowTest]
+        [Row(@"D:\**")]
+        [Row(@"D:\Development\**")]
+        [Row(@"D:\*\Project\Test.js")]
+        [Row(@"D:\**\Test.js")]
+        [Row(@"D:\*\Project\*.js")]
+        [Row(@"D:\*\Project\*.*")]
+        [Row(@"D:\Development\*\*.*")]
+        [Row(@"D:\Development/*/*.*")]
+        public void TestGetMatchesMatchesExpectedInput(string pattern) {
+            const string ExpectedMatch = @"D:\Development\Project\Test.js";
+            var support = PathBasedWildcardSupport();
+
+            Assert.AreEqual(
+                ExpectedMatch,
+                support.GetMatches(pattern, Enumerable.Repeat(ExpectedMatch, 1)).SingleOrDefault()
+            );
+        }
+
+        [RowTest]
+        [Row(@"D:\*\Test.js")]
+        [Row(@"D:\Development\Project\*")]
+        public void TestGetMatchesDoesNotMatchOverSeparators(string pattern) {
+            const string UnexpectedMatch = @"D:\Development\Project\Test.js";
+            var support = PathBasedWildcardSupport();
+
+            Assert.IsNull(support.GetMatches(pattern, Enumerable.Repeat(UnexpectedMatch, 1)).SingleOrDefault());
+        }
+
+        private WildcardSupport PathBasedWildcardSupport() {
+            return new WildcardSupport('/', '\\', '.');
+        }
+    }
+}
---
+++ MbUnit.JavaScript/References/WildcardSupport.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace MbUnit.JavaScript.References {
+    public class WildcardSupport {
+        private readonly char[] separators;
+        private readonly string escapedSeparators;
+        private readonly Regex wildcardRootRegex;
+        private readonly Regex substitutionRegex;
+
+        public WildcardSupport(params char[] separators) {
+            this.separators = separators;
+            Array.Sort(this.separators);
+
+            this.escapedSeparators = Regex.Escape(new string(separators));
+            wildcardRootRegex = new Regex(this.GetRootPattern());
+            substitutionRegex = new Regex(string.Format(@"\*{{1,2}}|[{0}]|[^*{0}]+", this.escapedSeparators));
+        }
+
+        private string GetRootPattern() {
+            if (this.escapedSeparators.Length == 0) 
+                return "^(?:[^*]+)";
+
+            return string.Format(@"^((?:[^{0}]+[{0}]+)+)[^{0}]*\*?", this.escapedSeparators);
+        }
+
+        public bool HasWildcards(string pattern) {
+            return pattern.Contains("*");
+        }
+
+        public string GetFixedRoot(string pattern) {
+            return wildcardRootRegex.Match(pattern).Groups[1].Value;
+        }
+
+        public IEnumerable<string> GetMatches(string pattern, IEnumerable<string> list) {
+            var regexString = substitutionRegex.Replace(pattern, match => SubstitutePattern(match.Value));
+            var regex = new Regex("^" + regexString + "$");
+
+            foreach(var item in list) {
+                if (!regex.IsMatch(item))
+                    continue;
+
+                yield return item;
+            }
+        }
+
+        private string SubstitutePattern(string pattern) {
+            if (pattern == "**")
+                return ".*";
+
+            if (pattern == "*")
+                return "[^" + this.escapedSeparators + "]*";
+
+            if (pattern.Length == 1 && Array.BinarySearch(this.separators, pattern[0]) > -1)
+                return "[" + this.escapedSeparators + "]";
+
+            return Regex.Escape(pattern);
+        }
+    }
+}
