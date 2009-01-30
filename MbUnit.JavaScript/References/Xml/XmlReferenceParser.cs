702b069fad3d696447230b84e7658d610019a7d4
ashmind <ashmind@1544f0e1-4d48-0410-9c11-bd241b4472a7>
2009-01-30 12:41:15 +0000

    Fixed crash on non-xml comments starting with ///.

--- MbUnit.JavaScript/References/Xml/XmlReferenceParser.cs
+++ MbUnit.JavaScript/References/Xml/XmlReferenceParser.cs
@@ -28,19 +28,20 @@ using System;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml;
 using System.Xml.XPath;
 
 namespace MbUnit.JavaScript.References.Xml {
     public class XmlReferenceParser {
         public static XmlReferenceParser Default { get; private set; }
-        private static readonly Regex CommentRegex = new Regex(@"^\s*///\s*(.*)");
+        private static readonly Regex CommentRegex = new Regex(@"^\s*///\s*(<reference.*)");
 
         static XmlReferenceParser() {
             Default = new XmlReferenceParser();
         } 
 
         public IXPathNavigable Parse(string script) {
-            string xmlString = this.ExtractReferencesString(script);
+            var xmlString = this.ExtractReferencesString(script);
             XPathDocument xml;
             using (var reader = new StringReader(xmlString)) {
                 xml = new XPathDocument(reader);
@@ -50,7 +51,7 @@ namespace MbUnit.JavaScript.References.Xml {
         }
 
         private string ExtractReferencesString(string script) {
-            string[] lines = script.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var lines = script.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
             var references = new StringBuilder("<references>");
 
             foreach (var line in lines) {
